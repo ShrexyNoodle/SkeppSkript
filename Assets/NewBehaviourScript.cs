738584a48ab8cfaea2661b5d6ed46a5165eb8c37
simon.korsgaardtuon <simon.korsgaardtuong@elev.ga.lbs.se>
2018-10-05 15:59:41 +0200

    Added ship, will begin code later.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript: MonoBehaviour
+{
+    public int spaceShipStartSpeed;
+    [Range(-100, 100)]
+    public float spaceShipRotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(spaceShipStartSpeed, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+        { 
+         
+
+        }
+        
+    }
+}
